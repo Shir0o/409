dbb845e93701099d0d521c2ed7c68c20b0d3ae57
shir0o <2931344974@qq.com>
2018-10-03 19:08:27 -0400

    75% training and 25% training

--- Project 2/WAN.py
+++ Project 2/WAN.py
@@ -1,15 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
 file = open("data.txt", "r")
 
 height_male, weight_male, height_female, weight_female = ([] for _ in range(4))
-true_negative, false_negative, true_positive, false_positive = (0 for _ in range(4))
-total_error = 1
-count = 0
-
-# neuron_weights = [0.141, 1, -92.84]
-neuron_weights = [1, 1, 1]
+neuron_weights = []
 
 for _ in range(2000):
     data = file.readline().split(", ")
@@ -37,69 +33,77 @@ def graph():
     plt.show()
 
 
-while total_error > 10 ** -5 or count < 1000:
-    total_error = 0
-    count += 1
-    print(count)
+def perceptron(training, iteration):
+    global neuron_weights
+    neuron_weights = [random.randint(1, 100), random.randint(1, 100), random.randint(1, 100)]
 
-    for i in range(1500):
+    true_negative, false_negative, true_positive, false_positive = (0 for _ in range(4))
+    total_error = 1
+    count = 0
+
+    print("Training for " + str(training) + " training data...")
+
+    while total_error > 10 ** -5 and count < iteration:
+        total_error = 0
+        count += 1
+
+        for i in range(training):
+            net = (height_male[i] * neuron_weights[0]) + (weight_male[i] * neuron_weights[1]) + neuron_weights[2]
+
+            desire = 1
+            actual = 1 if net >= 0 else -1
+            error = desire - actual
+            correction = 0.3 * error
+
+            neuron_weights[0] += correction * height_male[i]
+            neuron_weights[1] += correction * weight_male[i]
+            neuron_weights[2] += correction
+
+            total_error += 1 / 4000 if net < 0 else 0
+
+            net = (height_female[i] * neuron_weights[0]) + (weight_female[i] * neuron_weights[1]) + neuron_weights[2]
+
+            desire = -1
+            actual = -1 if net < 0 else 1
+            error = desire - actual
+            correction = 0.3 * error
+
+            neuron_weights[0] += correction * height_female[i]
+            neuron_weights[1] += correction * weight_female[i]
+            neuron_weights[2] += correction
+
+            total_error += 1 / 4000 if net >= 0 else 0
+
+    for i in range(training, 2000):
         net = (height_male[i] * neuron_weights[0]) + (weight_male[i] * neuron_weights[1]) + neuron_weights[2]
 
-        desire = 1
-        actual = 1 if net >= 0 else -1
-        error = desire - actual
-        correction = 0.5 * error
-
-        neuron_weights[0] += correction * height_male[i]
-        neuron_weights[1] += correction * weight_male[i]
-        neuron_weights[2] += correction
-
-        total_error += 1 / 4000 if net < 0 else 0
-        # graph()
+        if net >= 0:
+            true_positive += 1
+        elif net < 0:
+            false_negative += 1
 
         net = (height_female[i] * neuron_weights[0]) + (weight_female[i] * neuron_weights[1]) + neuron_weights[2]
 
-        desire = -1
-        actual = -1 if net < 0 else 1
-        error = desire - actual
-        correction = 0.5 * error
+        if net < 0:
+            true_negative += 1
+        elif net >= 0:
+            false_positive += 1
 
-        neuron_weights[0] += correction * height_female[i]
-        neuron_weights[1] += correction * weight_female[i]
-        neuron_weights[2] += correction
+    accuracy = (true_positive + true_negative) / (true_negative + true_positive + false_negative + false_positive)
+    error = 1 - accuracy
 
-        total_error += 1 / 4000 if net >= 0 else 0
-        # graph()
+    print("")
+    print("True Positive:", true_positive)
+    print("False Positive:", false_positive)
+    print("True Negative:", true_negative)
+    print("False Negative:", false_negative)
+    print("Accuracy: ", accuracy)
+    print("Error: ", error)
+    print("")
 
-    print(total_error)
+    print(neuron_weights)
+    graph()
 
-print(neuron_weights)
 
-for i in range(500):
-    net = (height_male[i] * neuron_weights[0]) + (weight_male[i] * neuron_weights[1]) + neuron_weights[2]
-
-    if net >= 0:
-        true_positive += 1
-    elif net < 0:
-        false_negative += 1
-
-    net = (height_female[i] * neuron_weights[0]) + (weight_female[i] * neuron_weights[1]) + neuron_weights[2]
-
-    if net < 0:
-        true_negative += 1
-    elif net >= 0:
-        false_positive += 1
-
-accuracy = (true_positive + true_negative) / (true_negative + true_positive + false_negative + false_positive)
-error = 1 - accuracy
-
-print("")
-print("True Positive:", true_positive)
-print("False Positive:", false_positive)
-print("True Negative:", true_negative)
-print("False Negative:", false_negative)
-print("Accuracy: ", accuracy)
-print("Error: ", error)
-print("")
-
-graph()
+perceptron(1500, 1000)
+perceptron(500, 1000)
