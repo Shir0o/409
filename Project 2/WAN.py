f4876f871bb5ff4cb987d75a8a01ded3094d4ddc
shir0o <2931344974@qq.com>
2018-10-07 17:06:45 -0400

    minor change

--- Project 2/WAN.py
+++ Project 2/WAN.py
@@ -13,9 +13,6 @@ for _ in range(2000):
     height = float(data[0])
     weight = float(data[1])
 
-    # height = (height - 69) / 2.8
-    # weight = (weight - 172) / 31.1
-
     height_male.append(height)
     weight_male.append(weight)
 
@@ -24,36 +21,8 @@ for _ in range(2000):
     height = float(data[0])
     weight = float(data[1])
 
-    # height = (height - 63.6) / 2.5
-    # weight = (weight - 143) / 32.3
-
     height_female.append(height)
     weight_female.append(weight)
-
-
-# height, weight = ([] for _ in range(2))
-# for _ in range(2000):
-#     data = file.readline().split(", ")
-#     height.append(float(data[0]))
-#     weight.append(float(data[1]))
-#
-# height_male = np.array(height)
-# weight_male = np.array(weight)
-#
-# height, weight = ([] for _ in range(2))
-# for _ in range(2000):
-#     data = file.readline().split(", ")
-#     height.append(float(data[0]))
-#     weight.append(float(data[1]))
-#
-# height_female = np.array(height)
-# weight_female = np.array(weight)
-#
-# height_male = (height_male - min(height_male)) / (max(height_male) - min(height_male))
-# weight_male = (weight_male - min(weight_male)) / (max(weight_male) - min(weight_male))
-#
-# height_female = (height_female - min(height_female)) / (max(height_female) - min(height_female))
-# weight_female = (weight_female - min(weight_female)) / (max(weight_female) - min(weight_female))
 
 
 def graph():
@@ -72,7 +41,6 @@ def graph():
 
 
 def activation(soft, male, net):
-    # print(net)
     if soft:
         return np.tanh(0.5 * net)
     else:
@@ -86,7 +54,6 @@ def perceptron(training, iteration, soft):
     global neuron_weights
     neuron_weights = [round(random.uniform(-0.5, 0.5), 2), round(random.uniform(-0.5, 0.5), 2),
                       round(random.uniform(-0.5, 0.5), 2)]
-    print(neuron_weights)
 
     true_negative, false_negative, true_positive, false_positive = (0 for _ in range(4))
     total_error = 1
@@ -125,9 +92,6 @@ def perceptron(training, iteration, soft):
 
             total_error += 1 / 4000 if net >= 0 else 0
 
-        # print(total_error)
-        # print(count)
-
     for i in range(training + 1, 2000):
         net = (height_male[i] * neuron_weights[0]) + (weight_male[i] * neuron_weights[1]) + neuron_weights[2]
 
@@ -146,20 +110,20 @@ def perceptron(training, iteration, soft):
     accuracy = (true_positive + true_negative) / (true_negative + true_positive + false_negative + false_positive)
     error = 1 - accuracy
 
-    print("\n")
+    print("")
     print("True Positive:", true_positive)
     print("False Positive:", false_positive)
     print("True Negative:", true_negative)
     print("False Negative:", false_negative)
     print("Accuracy: ", accuracy)
     print("Error: ", error)
-    print("\n")
+    print("")
 
     graph()
 
 
-# perceptron(1500, 2000, False)
-# perceptron(500, 2000, False)
+perceptron(1500, 2000, False)
+perceptron(500, 2000, False)
 
 perceptron(1500, 2000, True)
 perceptron(500, 2000, True)
