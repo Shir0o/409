2a32cec25e8d461592a5f99c7121998537f7c547
shir0o <2931344974@qq.com>
2018-11-20 10:18:35 -0500

    linear

--- Project 3/WAN.py
+++ Project 3/WAN.py
@@ -1,12 +1,7 @@
-import operator
 import random
 
-import numpy as np
 import matplotlib.pyplot as plt
-
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.preprocessing import PolynomialFeatures
+import numpy as np
 
 file = open("train_data_1.txt", "r")
 time, energy = ([] for _ in range(2))
@@ -16,21 +11,22 @@ for _ in range(16):
     time.append(float(data[0]))
     energy.append(float(data[1]))
 
-weights = [round(random.uniform(-0.5, 0.5), 2), round(random.uniform(-0.5, 0.5), 2),
-           round(random.uniform(-0.5, 0.5), 2), round(random.uniform(-0.5, 0.5), 2)]
+weights = [round(random.uniform(-0.5, 0.5), 2), round(random.uniform(-0.5, 0.5), 2)]
 
 for i in range(16):
-    net = ((time[i] ** 3) * weights[0]) + ((time[i] ** 2) * weights[1]) + (time[i] * weights[2]) + weights[3]
+    time[i] = (time[i] - np.mean(time)) / np.std(time)
 
-    desire = energy[i]
-    actual = net
-    error = desire - actual
-    correction = 0.1 * error
+for _ in range(5000):
+    for i in range(16):
+        net = (time[i] * weights[0]) + weights[1]
 
-    weights[0] += correction * (time[i] ** 3)
-    weights[1] += correction * (time[i] ** 2)
-    weights[2] += correction * time[i]
-    weights[3] += correction
+        desire = energy[i]
+        actual = net
+        error = desire - actual
+        correction = 0.1 * error
+
+        weights[0] += correction * time[i]
+        weights[1] += correction
 
 print(weights)
 
@@ -38,9 +34,9 @@ plt.xlabel('Height')
 plt.ylabel('Weight')
 plt.scatter(time, energy)
 
-x = np.array(range(0, 20))
-formula = repr(weights[0]) + '*x**3+' + repr(weights[1]) + '*x**2+' + repr(weights[2]) + '*x+' + repr(weights[3])
+x = np.array(range(-2, 5))
+formula = repr(weights[0]) + '*x+' + repr(weights[1])
 y = eval(formula)
-plt.plot(x, y, 'k', alpha=0.3)
+plt.plot(x, y, 'k')
 
 plt.show()
