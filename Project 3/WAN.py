1c99a289599bd143a602f9c7ff70a25c0644cff4
shir0o <2931344974@qq.com>
2018-11-20 22:46:36 -0500

    test

--- Project 3/WAN.py
+++ Project 3/WAN.py
@@ -3,6 +3,21 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+def graph():
+    plt.xlabel('Height')
+    plt.ylabel('Weight')
+    plt.scatter(time, energy)
+
+    x = np.array(range(-2, 5))
+    formula = repr(weights[0]) + '*x**3+' + repr(weights[1]) + '*x**2+' + repr(weights[2]) + '*x+' + repr(weights[3])
+    # formula = repr(weights[0]) + '*x+' + repr(weights[1])
+    y = eval(formula)
+    plt.plot(x, y, 'k')
+
+    plt.show()
+
+
 file = open("train_data_1.txt", "r")
 time, energy = ([] for _ in range(2))
 
@@ -11,32 +26,38 @@ for _ in range(16):
     time.append(float(data[0]))
     energy.append(float(data[1]))
 
-weights = [round(random.uniform(-0.5, 0.5), 2), round(random.uniform(-0.5, 0.5), 2)]
+weights = [round(random.uniform(-0.5, 0.5), 2), round(random.uniform(-0.5, 0.5), 2),
+           round(random.uniform(-0.5, 0.5), 2), round(random.uniform(-0.5, 0.5), 2)]
+# weights = [round(random.uniform(-0.5, 0.5), 2), round(random.uniform(-0.5, 0.5), 2)]
 
 for i in range(16):
     time[i] = (time[i] - np.mean(time)) / np.std(time)
 
 for _ in range(5000):
+    total_error = 0
+
     for i in range(16):
-        net = (time[i] * weights[0]) + weights[1]
+        net = ((time[i] ** 3) * weights[0]) + ((time[i] ** 2) * weights[1]) + (time[i] * weights[2]) + weights[3]
+        # net = (time[i] * weights[0]) + weights[1]
 
         desire = energy[i]
         actual = net
         error = desire - actual
+        total_error += error ** 2
         correction = 0.1 * error
 
         weights[0] += correction * time[i]
-        weights[1] += correction
+        weights[1] += correction * time[i]
+        weights[2] += correction * time[i]
+        weights[3] += correction
+
+        # weights[0] += correction * time[i]
+        # weights[1] += correction
+
+        # graph()
+
+    print(total_error)
 
 print(weights)
 
-plt.xlabel('Height')
-plt.ylabel('Weight')
-plt.scatter(time, energy)
-
-x = np.array(range(-2, 5))
-formula = repr(weights[0]) + '*x+' + repr(weights[1])
-y = eval(formula)
-plt.plot(x, y, 'k')
-
-plt.show()
+graph()
